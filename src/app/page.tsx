c2ced26ab83d27487cf41589ba86ef50fc4f4708
Zeke Osborn <zekeosborn@outlook.com>
2024-10-12 10:30:40 +0700

    feat(root-page): redirect to home page if user is signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { auth } from '@/auth';
 import { Logo } from '@/components';
+import { redirect } from 'next/navigation';
 import SignInButton from './SignInButton';
 
-export default function Welcome() {
+export default async function Welcome() {
+  const session = await auth();
+
+  if (session) return redirect('/home');
+
   return (
     <main className="flex h-dvh items-center justify-center">
       <div className="flex flex-col items-center gap-14">
